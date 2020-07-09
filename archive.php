e5a41e312f9c6c8f28c409d0554524375d558d8d
Raj K. Singh <43915031+rajksingh2412@users.noreply.github.com>
2020-07-10 00:55:30 +0530

    Add files via upload

---
+++ archive.php
@@ -0,0 +1,30 @@
+<?php get_header();?>
+
+<section class="page-wrap">
+
+    <div class="container">
+
+
+        <section class="row">
+
+                <div class="col-lg-3">
+                
+                        <?php if( is_active_sidebar('blog-sidebar') ):?>
+                                    <?php dynamic_sidebar('blog-sidebar');?>
+                        <?php endif;?>
+                </div>
+                <div class="col-lg-9">
+                        <h3> <?php echo single_cat_title();?></h3>
+
+                        <?php get_template_part('includes/section','archive');?>
+                
+                        <?php previous_posts_link();?>
+                        <?php next_posts_link();?>
+                </div>
+        </section>
+    </div>
+
+</section>
+
+
+<?php get_footer();?>
